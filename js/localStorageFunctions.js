4392af3cbca007c11c39fc36d813486d85f9cd23
NFTGameA <joehirest72@gmail.com>
2024-12-23 01:21:08 +0700

    fix

--- js/localStorageFunctions.js
+++ js/localStorageFunctions.js
@@ -6,7 +6,25 @@ Storage.prototype.getObj = function (key) {
   return JSON.parse(this.getItem(key));
 };
 
+const enableFullScreen = () => {
+  const isTelegramWebApp = window.Telegram && window.Telegram.WebApp;
+  if (isTelegramWebApp) {
+    tg.expand();
+    tg.enableClosingConfirmation();
+
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      document.documentElement.msRequestFullscreen();
+    }
+  }
+};
+
 const saveGameDataToFirebase = (userData, gameData) => {
+  enableFullScreen();
+
   const cloudDB = firebase.firestore();
   const isTelegramWebApp = window.Telegram && window.Telegram.WebApp;
   const user = isTelegramWebApp ? tg.initDataUnsafe?.user : null;
@@ -15,62 +33,70 @@ const saveGameDataToFirebase = (userData, gameData) => {
     cloudDB
       .collection("Database")
       .doc(user.id.toString())
-      .update({
-        Score: gameData.scoreing,
-        Level: level,
-        Lives: lives,
-        Stars: seaStarNum,
-        Growth: ((score - (level - 1) * 30) / 30) * 100,
-        LastUpdated: firebase.firestore.FieldValue.serverTimestamp(),
-      })
-      .then(() => console.log("Game data updated for", user.username))
-      .catch((err) => console.error("Error updating data:", err));
+      .set(
+        {
+          Username: user.username || `User${user.id}`,
+          PhotoURL: user.photo_url || "images/Logo .png",
+          TelegramID: user.id,
+          FirstName: user.first_name || "",
+          LastName: user.last_name || "",
+
+          Score: gameData.scoreing || 0,
+          Level: level,
+          Lives: lives,
+          Stars: seaStarNum,
+          Growth: ((score - (level - 1) * 30) / 30) * 100,
+          LastUpdated: firebase.firestore.FieldValue.serverTimestamp(),
+
+          Level1Time: currentPlayerLevel1Time,
+          Level2Time: currentPlayerLevel2Time,
+          Level3Time: currentPlayerLevel3Time,
+
+          Badges: currentPlayerTempBadge,
+        },
+        { merge: true }
+      )
+      .then(() => console.log("Game data saved for", user.username))
+      .catch((err) => console.error("Error saving data:", err));
   }
 };
 
 let updateLocalStorage = function () {
+  const user = window.Telegram?.WebApp?.initDataUnsafe?.user;
+  if (!user) return;
+
+  const userID = user.id.toString();
+
   if (gameCompleteFlag) {
     if (lives > previousState.numberOfLives) {
       previousState.numberOfLives = lives;
-      localStorage.setObj(playerNa.value, previousState);
+      localStorage.setObj(userID, previousState);
     }
-    if (
-      currentPlayerLevel3Time < localStorage.getObj(playerNa.value).level3time
-    ) {
+    if (currentPlayerLevel3Time < previousState.level3time) {
       previousState.level3time = currentPlayerLevel3Time;
-      localStorage.setObj(playerNa.value, previousState);
+      localStorage.setObj(userID, previousState);
     }
-    saveGameDataToFirebase(playerNa.value, previousState);
+    saveGameDataToFirebase(userID, previousState);
   } else if (lives === 0) {
     if (score > previousState.scoreing) {
       previousState.scoreing = score;
-      localStorage.setObj(playerNa.value, previousState);
-      saveGameDataToFirebase(playerNa.value, previousState);
+      localStorage.setObj(userID, previousState);
+      saveGameDataToFirebase(userID, previousState);
     }
   } else {
     switch (level) {
       case 2:
-        {
-          if (
-            currentPlayerLevel1Time <
-            localStorage.getObj(playerNa.value).level1time
-          ) {
-            previousState.level1time = currentPlayerLevel1Time;
-            localStorage.setObj(playerNa.value, previousState);
-            saveGameDataToFirebase(playerNa.value, previousState);
-          }
+        if (currentPlayerLevel1Time < previousState.level1time) {
+          previousState.level1time = currentPlayerLevel1Time;
+          localStorage.setObj(userID, previousState);
+          saveGameDataToFirebase(userID, previousState);
         }
         break;
       case 3:
-        {
-          if (
-            currentPlayerLevel2Time <
-            localStorage.getObj(playerNa.value).level2time
-          ) {
-            previousState.level2time = currentPlayerLevel2Time;
-            localStorage.setObj(playerNa.value, previousState);
-            saveGameDataToFirebase(playerNa.value, previousState);
-          }
+        if (currentPlayerLevel2Time < previousState.level2time) {
+          previousState.level2time = currentPlayerLevel2Time;
+          localStorage.setObj(userID, previousState);
+          saveGameDataToFirebase(userID, previousState);
         }
         break;
     }
